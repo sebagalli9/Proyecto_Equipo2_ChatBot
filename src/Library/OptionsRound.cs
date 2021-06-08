9fb3f2ad20888d6c3bbf38fc50048b5795e662a7
Cotidib <constanza.dibueno@gmail.com>
2021-06-08 14:36:12 -0300

    Agrego comentarios

--- src/Library/OptionsRound.cs
+++ src/Library/OptionsRound.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 namespace Library
 {
     /*
+    Actualmente la clase OptionsRound cumple con el principio SRP ya que la única razón de cambio es modificar
+    la forma en la que se genera cada ronda nueva de opciones.
+
+    La clase OptionsRound no cumple con el patrón Expert ya que para conocer la información necesaria para crear 
+    cada nueva ronda de opciones va a tener que acudir a la clase Experta en conocer la lista de categorias 
+    (la clase que implemente la interfaz IReader)
     */
     public class OptionsRound
     {
